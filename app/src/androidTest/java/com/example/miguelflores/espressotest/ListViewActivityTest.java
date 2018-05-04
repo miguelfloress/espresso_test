f891638f9b5223deaac91773181837ddd6605a23
miguel.flores <Arturo.X.Millan.-nd@Disney.com>
2018-05-04 17:54:17 -0500

    add Tests

---
+++ app/src/androidTest/java/com/example/miguelflores/espressotest/ListViewActivityTest.java
@@ -0,0 +1,58 @@
+package com.example.miguelflores.espressotest;
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.example.miguelflores.espressotest.activities.ListViewActivity;
+import com.example.miguelflores.espressotest.base.BaseTest;
+import com.example.miguelflores.espressotest.util.MyUtilValidator;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.example.miguelflores.espressotest.util.ListSizeMatcher.hasListSize;
+
+/**
+ * @author miguel.flores.
+ */
+@RunWith(AndroidJUnit4.class)
+public class ListViewActivityTest extends BaseTest {
+
+    @Rule
+    public ActivityTestRule<ListViewActivity> mainActivityRule = new ActivityTestRule<>(ListViewActivity.class);
+    private ViewInteraction listView;
+    private ViewInteraction textViewSelection;
+
+    @Before
+    public void setUp() {
+        listView = onView(withId(R.id.listView));
+        textViewSelection = onView(withId(R.id.textView_selection));
+    }
+
+    @Test
+    public void checkInitialState() {
+        listView.check(matches(hasListSize(20)));
+        textViewSelection.check(matches(withText("")));
+    }
+
+    @Test
+    public void clickOnItem() {
+        listView.perform(click());
+        checkTextSelection("List item 1");
+        checkTextSelection("List item 5");
+        checkTextSelection("List item 15");
+    }
+
+    private void checkTextSelection(String text) {
+        MyUtilValidator.getDataInteraction(String.class, text).perform(click());
+        textViewSelection.check(matches(withText(text)));
+    }
+}
---
+++ app/src/androidTest/java/com/example/miguelflores/espressotest/RecyclerViewActivityTest.java
@@ -0,0 +1,66 @@
+package com.example.miguelflores.espressotest;
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.example.miguelflores.espressotest.activities.RecyclerViewActivity;
+import com.example.miguelflores.espressotest.base.BaseTest;
+import com.example.miguelflores.espressotest.util.ListSizeMatcher;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.example.miguelflores.espressotest.util.MyViewAction.performClickOnChildWithId;
+
+/**
+ * @author miguel.flores.
+ */
+@RunWith(AndroidJUnit4.class)
+public class RecyclerViewActivityTest extends BaseTest {
+
+    @Rule
+    public ActivityTestRule<RecyclerViewActivity> mainActivityRule = new ActivityTestRule<>(RecyclerViewActivity.class);
+    private ViewInteraction recyclerView;
+    private ViewInteraction textViewSelection;
+
+    @Before
+    public void setUp() {
+        recyclerView = onView(withId(R.id.recyclerView));
+        textViewSelection = onView(withId(R.id.textView_selection));
+    }
+
+    @Test
+    public void checkInitialState() {
+        recyclerView.check(matches(ListSizeMatcher.hasListSize(20)));
+        textViewSelection.check(matches(withText("")));
+    }
+
+    @Test
+    public void checkItemsClicked() {
+        recyclerView.perform(click());
+        checkRecyclerViewSelection(19, "Recycler Item Number 19");
+        checkRecyclerViewSelection(1, "Recycler Item Number 1");
+        checkRecyclerViewSelection(5, "Recycler Item Number 5");
+        checkRecyclerViewSelection(17, "Recycler Item Number 17");
+    }
+
+    private void checkRecyclerViewSelection(int pos, String value) {
+        recyclerView.perform(RecyclerViewActions.actionOnItemAtPosition(pos,
+                performClickOnChildWithId(R.id.textView)));
+
+        /* uncomment this to show what happens
+        recyclerView.perform(RecyclerViewActions.scrollToPosition(pos))
+                    .check(matches(withId(R.id.textView))).perform(click());
+        */
+        textViewSelection.check(matches(withText(value)));
+    }
+}
